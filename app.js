03244856ef1f02cc34b981815b6ebdf64d17b358
SirKittyton <gregcrutchlow@gmail.com>
2022-10-23 00:43:58 -0400

    added styling

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// make the nav bar solid when scrolling
+document.addEventListener('click', function handleClick(event) {
+    event.target.classList.toggle('open');
+    event.target.classlist.toggle('showMenu');
+});
 
-// when "check out my projects" is clicked, "scroll" down to projects, don't jump there
-
-// have hidden hamburger menu appear when window width goes to mobile size
-
-
-
-function scrollToHash(hashName) { location.hash = "#" + hashName; }
-
-{/* <i class="fa-brands fa-html5"></i>
-<i class="fa-brands fa-css3-alt"></i>
-<i class="fa-brands fa-js"></i>
-<i class="fa-brands fa-sass"></i>
-<i class="fa-brands fa-git-alt"></i>
-<i class="fa-brands fa-github"></i>
-<i class="fa-brands fa-react"></i>
-<i class="fa-solid fa-code-branch"></i>
-<i class="fa-solid fa-pen-ruler"></i> */}
