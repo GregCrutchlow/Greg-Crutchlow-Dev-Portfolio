93b74e686d25b79ede499d88eed053c98e3bb333
SirKittyton <gregcrutchlow@gmail.com>
2022-10-23 23:29:31 -0400

    added styling and media queries

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-document.addEventListener('click', function handleClick(event) {
+const navShow = document.querySelector('.hiddenNav');
+const hamburgerMenu = document.querySelector('.mobileMenu');
+
+hamburgerMenu.addEventListener('click', function handleClick(event) {
     event.target.classList.toggle('open');
     event.target.classlist.toggle('showMenu');
 });
 
+hamburgerMenu.addEventListener('click', () => {
+    navShow.classList.toggle('show');
+})
