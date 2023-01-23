71b7aeadb4e723a6ee9a031c122645d8f35b2bc5
GregCrutchlow <gregcrutchlow@gmail.com>
2023-01-23 11:29:00 -0500

    fixed styling for navBar and added scrolling effect and changed background on header and contact sections

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const navShow = document.querySelector('.hiddenNav');
-const hamburgerMenu = document.querySelector('.mobileMenu');
-const headerPic = document.querySelector('.headerImg')
+const portfolio = {};
 
-const icon = document.getElementById('icon');
-const bar1 = document.getElementById('bar1');
-const bar2 = document.getElementById('bar2');
-const bar3 = document.getElementById('bar3');
-const nav = document.getElementById('nav');
+portfolio.navBarToggle = () => {
+    const navBar = document.querySelector(".navBar");
 
-icon.addEventListener('click', () => {
-    bar1.classList.toggle('bar1')
-    bar2.classList.toggle('bar2')
-    bar3.classList.toggle('bar3')
-    nav.classList.toggle('show')
-    icon.classList.toggle('open')
-})
+    window.addEventListener('scroll', function () {
+        if (this.window.scrollY > navBar.clientHeight + 750) {
+            navBar.classList.add('scrolled')
+        } else {
+            navBar.classList.remove('scrolled')
+        }
+    })
+}
 
-fu
+portfolio.init = () => {
+    portfolio.navBarToggle();
+}
+
+portfolio.init();
